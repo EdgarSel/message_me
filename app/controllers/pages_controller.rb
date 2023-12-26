43fc71f51e7174825686f3f77b9d1eb5b94f951e
Serrano Sel <selperaza8@gmail.com>
2023-12-26 11:06:40 -0600

    Primer commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  before_action :require_user
+
   def home
+    @user = User.all
+    @message = Message.new
+    @messages = Message.custom_display
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,32 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
   def new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Tu haz iniciado sesion con exito"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Había algún problema con su información de inicio de sesión"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Tu session se a cerrado con exito"
+    redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "Ya has iniciado sesión"
+      redirect_to root_path
+    end
   end
 end
